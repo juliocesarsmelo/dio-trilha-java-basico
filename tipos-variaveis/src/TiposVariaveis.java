69f1acd1574e1e002d441ae3c32aac71a0b61f82
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2025-12-14 01:55:13 -0300

    finalizando aula

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        // [PRIMITIVOS]
+
+        //Inteiros
+        byte idade = 20;
+        short ano = 2025;
+        int cep = 70000000;
+        long cpf = 98765432109L;
+
+        //Pontos Flutuantes
+        float pi = 3.4F;
+        double salario = 1275.33;
+
+        //Lógico
+        boolean verdadeiro = true;
+
+        //Caracter
+        char nome = 'A';
+
+        //[CONSTANTES]
+        final double VALOR_DE_PI = 3.14;
+
+
+
     }
 }
+
