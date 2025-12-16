3ddc7c0b83e0b1bf56d2974efa0953f325f2f630
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2025-12-16 00:53:42 -0300

    finalizando aula de Javadoc

---
+++ sintaxe-java/src/edu/julio/fundamentos_plataforma/JavaDoc.java
@@ -0,0 +1,27 @@
+package edu.julio.fundamentos_plataforma;
+
+/**
+ * Comentário para documentação
+ * 
+ * Documentação sobre a classe
+ * 
+ * @author Júlio Melo
+ * @version 1.0
+ * @since 12/25
+ * 
+ */
+
+public class JavaDoc {
+    
+    //Comentário em linha
+
+    /*
+    * Comentário em bloco
+    */
+
+    //Para criar o Javadoc 
+    //
+    // javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs 
+    // (Get-ChildItem -Recurse src/edu/julio -Filter *.java | ForEach-Object { $_.FullName }) 
+
+}
