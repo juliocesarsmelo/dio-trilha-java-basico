70c20ec223180e77d214ac15099d652565ea1bcb
juliocesarsmelo <juliocesarmelo.pro@gmail.com>
2025-12-21 23:15:54 -0300

    iniciando aula de estruturas de repeticao

---
+++ estruturas-de-controle/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    
+    public static void main(String[] args) {
+        
+        for(int cont = 1; cont <= 20; cont ++){
+            System.out.println("Carneirinho nº " + cont);
+        }
+        System.out.println("Dormiu ......");
+    }
+}
